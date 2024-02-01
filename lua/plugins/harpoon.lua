cdac92b8a15064a5466052c1d802fa6718622253
Jacob Svensmark <jacobsvensmark@bac1-q202-co1.clients.net.dtu.dk>
2024-02-01 15:48:47 +0100

    updated harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,10 +3,19 @@ return {
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
-        vim.keymap.set("n", "<leader>a", mark.add_file)
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+        local harpoon = require("harpoon")
+        -- REQUIRED
+        harpoon:setup()
+        -- REQUIRED
+
+        --local mark = require("harpoon.mark")
+        --local ui = require("harpoon.ui")
+
+        --vim.keymap.set("n", "<leader>a", mark.add_file)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        --vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end) 
+
         vim.keymap.set("n", "<C-m>", function() ui.nav_next() end)
         vim.keymap.set("n", "<C-n>", function() ui.nav_prev() end)
     end
