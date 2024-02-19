ed52548d367211aed88a7992d2a3535f94ab2a9a
Jacob Svensmark <jacobsvensmark@Jacobs-MacBook-Pro.local>
2024-02-19 11:31:21 +0100

    Removed a couple of blanklines - test basically

--- lua/plugins/ibl.lua
+++ lua/plugins/ibl.lua
@@ -3,14 +3,11 @@ return {
     config = function()
         local scope = "focus"
         local indent = "passive"
-
         local hooks = require("ibl.hooks")
-
         hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
             vim.api.nvim_set_hl(0, "focus", { fg = "#7486bd" })
             vim.api.nvim_set_hl(0, "passive", { fg = "#41425e" })
         end)
-
         require("ibl").setup({
             indent = { highlight = indent },
             --            node_type = { fortran = { "MODULE", "SUBROUTINE", "PROGRAM", "LOOP" } },
